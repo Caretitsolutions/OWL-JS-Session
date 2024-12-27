503a95a8430aef10030d97737d94bff4f7dcf968
Urvisha Buha <urvisha.buha@caretit.com>
2024-12-27 06:58:46 +0530

    [MOD] layout component

--- expense_tracker/__manifest__.py
+++ expense_tracker/__manifest__.py
@@ -11,8 +11,21 @@
     ],
     'assets': {
         'web.assets_backend': [
-            'expense_tracker/static/src/**/*',
+'expense_tracker/static/src/expense_tracker.js',
+'expense_tracker/static/src/expense_tracker.xml',
+'expense_tracker/static/src/components/**/*.js',
+'expense_tracker/static/src/gallery_controller.js',
+# 'expense_tracker/static/src/gallery_rendered.xml',
+# 'expense_tracker/static/src/gallery_renderer.js',
+'expense_tracker/static/src/gallery_view.js',
+'expense_tracker/static/src/gallery_controller.xml',
+# 'expense_tracker/static/src/views.xml',
+            # 'expense_tracker/static/src/gallery_renderer.js',
+            # 'expense_tracker/static/src/**/*',
             'expense_tracker/static/src/main.js',
+        ],
+'web.assets_qweb': [
+            'expense_tracker/static/src/views.xml',
         ],
     },
     'installable': True,
--- gallery/static/src/gallery_controller.js
+++ gallery/static/src/gallery_controller.js
@@ -12,50 +12,50 @@ export class GalleryController extends Component {
     static template = "gallery.GalleryController";
     static props = {
         ...standardViewProps,
-                archInfo: Object,
+//                archInfo: Object,
 
     };
 
 
-    static components = { Layout, GalleryRenderer };
+    static components = { Layout };
 
-    setup() {
-        this.orm = useService("orm");
-        this.model = useState(
-            new GalleryModel(
-                this.orm,
-                this.props.resModel,
-                this.props.fields,
-                this.props.archInfo,
-            )
-        );
-        this.images = useState({ data: [] });
-         usePager(() => {
-            return {
-                offset: this.model.pager.offset,
-                limit: this.model.pager.limit,
-                total: this.model.recordsLength,
-                onUpdate: async ({ offset, limit }) => {
-                    this.model.pager.offset = offset;
-                    this.model.pager.limit = limit;
-                    await this.model.load(this.props.domain);
-                },
-            };
-         });
-        onWillStart(async () => {
-//            const { records } = await this.loadImages(this.props.domain);
-//            this.images.data = records;
-                        await this.model.load(this.props.domain);
-
-        });
-        onWillUpdateProps(async (nextProps) => {
-            if (JSON.stringify(nextProps.domain) !== JSON.stringify(this.props.domain)) {
-//                const { records } = await this.loadImages(nextProps.domain);
-//                this.images.data = records;
-                                await this.model.load(nextProps.domain);
-            }
-        });
-    }
+//    setup() {
+//        this.orm = useService("orm");
+//        this.model = useState(
+//            new GalleryModel(
+//                this.orm,
+//                this.props.resModel,
+//                this.props.fields,
+//                this.props.archInfo,
+//            )
+//        );
+//        this.images = useState({ data: [] });
+//         usePager(() => {
+//            return {
+//                offset: this.model.pager.offset,
+//                limit: this.model.pager.limit,
+//                total: this.model.recordsLength,
+//                onUpdate: async ({ offset, limit }) => {
+//                    this.model.pager.offset = offset;
+//                    this.model.pager.limit = limit;
+//                    await this.model.load(this.props.domain);
+//                },
+//            };
+//         });
+//        onWillStart(async () => {
+////            const { records } = await this.loadImages(this.props.domain);
+////            this.images.data = records;
+//                        await this.model.load(this.props.domain);
+//
+//        });
+//        onWillUpdateProps(async (nextProps) => {
+//            if (JSON.stringify(nextProps.domain) !== JSON.stringify(this.props.domain)) {
+////                const { records } = await this.loadImages(nextProps.domain);
+////                this.images.data = records;
+//                                await this.model.load(nextProps.domain);
+//            }
+//        });
+//    }
 
 
 }
