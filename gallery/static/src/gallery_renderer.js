29d43b016c45a4363937ced8cf80ff0a0d08dc33
Urvisha Buha <urvisha.buha@caretit.com>
2024-12-27 07:12:27 +0530

    [MOD] add data

--- gallery/static/src/gallery_renderer.js
+++ gallery/static/src/gallery_renderer.js
@@ -6,7 +6,7 @@ import { GalleryImage } from "./gallery_image";
 
 
 export class GalleryRenderer extends Component {
-    static components = { GalleryImage };
+//    static components = { GalleryImage };
     static template = "awesome_gallery.GalleryRenderer";
     static props = {
         model: GalleryModel,
